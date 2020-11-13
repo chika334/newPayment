26968e119a4b005d2aa7122c7cde83f6d8832d39
freshaluck <freshaluck@localhost.localdomain>
2020-11-13 09:07:18 -0600

    new backend

--- routes/wallet.js
+++ routes/wallet.js
@@ -9,8 +9,8 @@ router.get('/getWallet', auth, async (req, res) => {
 })
 
 router.post('/addFunds', (req, res) => {
-    const { valueamount } = req.body.amount
-    console.log(valueamount)
+    // const { valueamount } = req.body.amount
+    console.log(amount)
 })
 
 module.exports = router;
