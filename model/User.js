2fdc3913e617d3fd529bd27368e8297add9e97fd
freshaluck <freshaluck@localhost.localdomain>
2020-11-13 06:09:35 -0600

    nice work

--- model/User.js
+++ model/User.js
@@ -26,43 +26,10 @@ const UserSchema = new mongoose.Schema({
 		default: 0
 	}
 }, { timestamp: true })
-
-UserSchema
-  .virtual('password')
-  .set(function(password) {
-    // create a temporary variable _password
-    this._password = password,
-    // generate salt
-    this.salt = this.makeSalt()
-    // encrypt password
-    this.Hashed_password = this.encryptPassword(password)
-  })
-  .get(function() {
-    return this._password
-  })
-
-UserSchema.methods = {
-  authenticate: function (plainText) {
-    // encrypt and compare with hashed password from the database
-    return this.encryptPassword(plainText) === this.Hashed_password;
-  },
-  encryptPassword: function(password) {
-    if (!password) return '';
-    try {
-      return crypto
-              .createHmac('sha1', this.salt)
-              .update(password)
-              .digest('hex')
-    } catch (err) {
-      return '';
-    }
-  },
-  makeSalt: function() {
-    return Math.round(new Date().valueOf() * Math.random()) + '';
-  },
-  generateAuthToken: function() {
-    return jwt.sign({_id: this._id}, process.env.JWT_SECRET)
-  }
+  
+UserSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, config.get('jwtPrivateKey'));
+  return token;
 }
 
 module.exports = mongoose.model('User', UserSchema)
--- routes/wallet.js
+++ routes/wallet.js
@@ -3,8 +3,8 @@ const express = require('express')
 const router = express.Router()
 const auth = require('../middleware/auth')
 
-router.get('/getWallet', auth, (req, res) => {
-    const wallet = Wallet.findById(req.user._id)
+router.get('/getWallet', auth, async (req, res) => {
+    const wallet = await Wallet.findById(req.user._id)
     console.log(wallet)
 })
 
