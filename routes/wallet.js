0ad930f9b92d8b64578157a8b02166d567dd08bd
freshaluck <freshaluck@localhost.localdomain>
2020-11-16 21:32:52 -0600

    all

--- routes/wallet.js
+++ routes/wallet.js
@@ -44,12 +44,13 @@ router.post('/deductFunds', auth, async (req, res) => {
     }
 
     await Wallet.findById({ _id: req.user.walletId }, (err, wallets) => {
-        if (err) throw err;
+        //if (err) throw err;
         if(wallets.wallet <= 0) {
             return res.status(400).json({
                 msg: "Can't complete transaction wallet balance low"
             });
         } 
+        
         return wallets.updateOne({ wallet: wallets.wallet - AmountInt }, (err, success) => {
             if (err) {
               return res.json({ error: console.log(err)})
