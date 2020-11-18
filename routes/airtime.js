9b793a1cd86dcf662a2e4f638e560a96c46a5c52
freshaluck <freshaluck@localhost.localdomain>
2020-11-17 18:41:26 -0600

    all

--- routes/airtime.js
+++ routes/airtime.js
@@ -3,11 +3,6 @@ const express = require('express')
 const router = express.Router()
 const auth = require('../middleware/auth')
 
-//router.get('/getWallet', auth, async (req, res) => {
-  //  const wallet = await Wallet.findById(req.user.walletId)
-    //res.json(wallet)
-//})
-
 router.post('/creditTransaction', (req, res) => {
     console.log(req.body)
 })
