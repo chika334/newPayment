57b3755d0d2527db5f1fcdbc194a2500486115ed
freshaluck <freshaluck@localhost.localdomain>
2020-11-19 08:06:44 -0600

    good

--- model/Transaction.js
+++ model/Transaction.js
@@ -1,30 +1,15 @@
 const mongoose = require("mongoose")
 
 const TransactionSchema = new mongoose.Schema({
-    product_name: {
-        type: String,
-    },
-    transactionId: {
-        type: String
-    },
-    amount: {
-        type: String
-    },
-    total_amount: {
-        type: String
-    },
-    requestId: {
-        type: String
-    },
-    date: {
-        type: String
-    },
-    amount: {
-        type: String
-    },
-    status: {
-        type: String
-    },
+    product_name: String,
+    transactionId: String,
+    amount: String,
+    total_amount: String,
+    requestId: String,
+    date: String,
+    amount: String,
+    status: String,
+    walletId: String
 }, { timestamp: true })
 
 module.exports = mongoose.model('Transaction', TransactionSchema)
--- routes/airtime.js
+++ routes/airtime.js
@@ -5,6 +5,7 @@ const auth = require('../middleware/auth')
 const axios = require('axios')
 const Pay = require('../model/PayRequest')
 const { v4: uuidv4 } = require('uuid');
+const Transaction = require("../model/Transaction")
 
 router.get('/getPayment', auth, async (req, res) => {
     const credit = await Pay.find({ walletId: req.user.walletId })
@@ -47,6 +48,48 @@ router.post('/creditTransaction', auth, async (req, res) => {
             })
 
             pay.save();
+            console.log(res.data)
+        })
+        .catch(err => console.log(err))
+        
+   res.status(200).json({
+       msg: 'success'
+   })
+})
+
+router.post('/Transaction', auth, async (req, res) => {
+    const { AmountInt, service, phone, name } = req.body
+    const requestId = uuidv4();
+
+    const user = `${process.env.email_login}:${process.env.password_login}`
+    const base64 = Buffer.from(user).toString('base64');
+
+    const config = {
+        headers: {
+          "Authorization": `Basic ${base64}`
+        }
+      }
+
+    const body = {
+        request_id: requestId,
+    }
+    
+    const userId = await Wallet.findById(req.user.walletId)
+
+    axios.post(`${process.env.specificTrans}`, body, config)
+        .then(res => {
+            const trans = new Transaction({
+                amount: res.data.amount,
+                requestId: res.data.requestId,
+                product_name: res.data.content.transactions.product_name,
+                date: res.data.transaction_date.date,
+                total_amount: res.data.content.transactions.total_amount,
+                transactionId: res.data.content.transactions.transactionId,
+                status: res.data.response_description,
+                walletId: userId._id,
+            })
+
+            trans.save();
             console.log(res.data)
         })
         .catch(err => console.log(err))
