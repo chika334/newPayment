556b7e50a130cb1c13c85c3ce39e9d81570e7209
freshaluck <freshaluck@localhost.localdomain>
2020-11-19 19:05:24 -0600

    all

--- routes/Electric.js
+++ routes/Electric.js
@@ -32,7 +32,7 @@ router.post('/verifyNumber', auth, async (req, res) => {
         })
         .catch(err => res.status(400).json({
             msg: `Incorrect meter number. Please try with a correct one`
-        })
+        }))
 })
 
 router.post('/Transaction', auth, async (req, res) => {
