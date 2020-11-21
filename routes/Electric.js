efe6977b125be284b693cc86b6dc26f7904e9cc1
freshaluck <freshaluck@localhost.localdomain>
2020-11-21 06:04:47 -0600

    new all

--- routes/Electric.js
+++ routes/Electric.js
@@ -58,45 +58,9 @@ router.post('/verifyNumber', auth, async (req, res, error) => {
     }
 })
 
-router.post('/Transaction', auth, async (req, res) => {
-    const { trans } = req.body
-
-    const user = `${process.env.email_login}:${process.env.password_login}`
-    const base64 = Buffer.from(user).toString('base64');
-
-    const config = {
-        headers: {
-          "Authorization": `Basic ${base64}`
-        }
-      }
-
-    const body = {
-        request_id: trans,
-    }
-    
-    const userId = await Wallet.findById(req.user.walletId)
-
-    axios.post(`${process.env.specificTrans}`, body, config)
-        .then(res => {
-            const trans = new Transaction({
-                amount: res.data.content.transactions.amount,
-                requestId: req.body.trans,
-                product_name: res.data.content.transactions.type,
-                date: res.data.transaction_date.date,
-                total_amount: res.data.content.transactions.total_amount,
-                transactionId: res.data.content.transactions.transactionId,
-                status: res.data.response_description,
-                walletId: userId._id,
-            })
-
-            trans.save();
-            console.log(res.data)
-        })
-        .catch(err => console.log(err))
-        
-   res.status(200).json({
-       msg: 'success'
-   })
+router.post('/prepaidMeterPayment', auth, async (req, res) => {
+    const { meter, service, select } = req.body
+    console.log(req.body)
 })
 
 module.exports = router;
