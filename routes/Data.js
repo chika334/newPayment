7a146e396f6745dbce736199ef5160d9594140ab
freshaluck <freshaluck@localhost.localdomain>
2020-11-21 18:44:05 -0600

    all

--- routes/Data.js
+++ routes/Data.js
@@ -8,8 +8,8 @@ const Data = require('../model/Data')
 const Transaction = require('../model/Transaction')
 
 router.get('/dataTransaction', auth, async (req, res) => {
-    const credit = await Data.find({ walletId: req.user.walletId })
-    res.status(200).json(credit)
+    const data = await Data.find({ walletId: req.user.walletId })
+    res.status(200).json(data)
 })
 
 router.post('/DataTransaction', auth, async (req, res) => {
