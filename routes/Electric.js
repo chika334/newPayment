6cbe98cdbb6d7cab3a79d1dbdfd0fed58532af24
freshaluck <freshaluck@localhost.localdomain>
2020-11-24 21:28:13 -0600

    all

--- routes/Electric.js
+++ routes/Electric.js
@@ -64,7 +64,7 @@ router.post('/verifyNumber', auth, async (req, res, error) => {
             })
             return;*/
         })
-        .catch(err => return err)
+        .catch(err => throw 404)
         .finally{
             if(response) {
                 console.log("Good")
