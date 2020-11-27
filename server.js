bd4f6704f6cb25556a0ceb786caf9f850bbcc852
freshaluck <freshaluck@localhost.localdomain>
2020-11-27 05:32:31 -0600

    all

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const cors = require("cors");
 const mongoose = require("mongoose")
 const express = require("express");
 const app = express();
-const router = express.Router()
-const multer = require('multer')
 require("dotenv").config()
 
 // routes
@@ -36,45 +34,6 @@ app.use('/api', electric)
 //app.use('/api', insurance);
 app.use('/api', data)
 app.use('/api', kyc)
-
-let storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/')
-  },
-  filename: function (req, file, cb) {
-    cb(null, `${Date.now()}_${file.originalname}`)
-  },
-  fileFilter: (req, file, cb) => {
-    const ext = path.extname(file.originalname)
-    if (ext !== '.jpg' && ext !== '.png' && ext !== '.mp4') {
-      return cb(res.status(400).end('only jpg, png, jpeg is allowed'), false);
-    }
-    cb(null, true)
-  }
-})
-
-app.use('/uploads', express.static('uploads'));
-
-const uploads = multer({ storage: storage })
-
-const uploaded = uploads.fields([{ name: 'caccertificate', maxCount: 1}, { name: 'idcard', maxCount: 1}, { name: 'bill', maxCount: 1}, { name: 'passport', maxCount: 1}])
-
-app.post("/api/companyUpdate", uploaded, (req, res) => {
-    const { user_id, companyname, companyaddress, homeaddress, alternatephone, localgov, State, identity, talk } = req.body
-    const { caccertificate, idcard, passport, bill } = req.files
-    
-    User.findOneAndUpdate(user_id, { companyname, companyaddress, homeaddress, alternatephone, localgov, State, identity, talk, caccertificate, passport, bill, idcard }, function (err, user) {
-        if (err || !user) {
-           return res.status(400).json({
-            msg: "User does not exist"
-        })
-       } else {
-         res.status(200).json({
-           msg: `Thanks for updating your profile`
-         });
-       }
-    })
-})
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
