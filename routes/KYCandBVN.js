fb7c4b10e1a50137cabf2383e2a3f9600bfcfffb
freshaluck <freshaluck@localhost.localdomain>
2020-11-27 05:20:19 -0600

    all

--- routes/KYCandBVN.js
+++ routes/KYCandBVN.js
@@ -24,43 +24,5 @@ router.post("/kyc-bvn", (req, res) => {
     })
 })
 
-let storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/')
-  },
-  filename: function (req, file, cb) {
-    cb(null, `${Date.now()}_${file.originalname}`)
-  },
-  fileFilter: (req, file, cb) => {
-    const ext = path.extname(file.originalname)
-    if (ext !== '.jpg' && ext !== '.png' && ext !== '.mp4') {
-      return cb(res.status(400).end('only jpg, png, jpeg is allowed'), false);
-    }
-    cb(null, true)
-  }
-})
-
-app.use('/uploads', express.static('uploads'));
-
-const uploads = multer({ storage: storage })
-
-const uploaded = uploads.fields([{ name: 'caccertificate', maxCount: 1}, { name: 'idcard', maxCount: 1}, { name: 'bill', maxCount: 1}, { name: 'passport', maxCount: 1}])
-
-router.post("/companyUpdate", uploaded, (req, res) => {
-    const { user_id, companyname, companyaddress, homeaddress, alternatephone, localgov, State, identity, talk } = req.body
-    const { caccertificate, idcard, passport, bill } = req.files
-    
-    User.findOneAndUpdate(user_id, { companyname, companyaddress, homeaddress, alternatephone, localgov, State, identity, talk, caccertificate, passport, bill, idcard }, function (err, user) {
-        if (err || !user) {
-           return res.status(400).json({
-            msg: "User does not exist"
-        })
-       } else {
-         res.status(200).json({
-           msg: `Thanks for updating your profile`
-         });
-       }
-    })
-})
 
 module.exports = router
