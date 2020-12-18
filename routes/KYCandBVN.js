3b4a10960e068fff76dd9c7b4db02ca41d46e752
chika334 <ikenyedaniel@gmail.com>
2020-12-18 04:51:49 +0100

    final

--- routes/KYCandBVN.js
+++ routes/KYCandBVN.js
@@ -10,54 +10,65 @@ router.post("/kyc-bvn", (req, res) => {
             msg: "Input all fields"
         })
     }
-    User.findOneAndUpdate(_id, { firstname, middlename, lastname, birthday, bvn, bvnphone }, function (err, user) {
+    User.findById(_id, function (err, user) {
         if (err || !user) {
-           return res.status(400).json({
-            msg: "User does not exist"
+            return res.status(400).json({
+                msg: "User does not exist"
+            })
+        }
+        
+        user.updateOne({ firstname, middlename, lastname, birthday, bvn, bvnphone }, (err, success) => {
+            if (err) {
+               return res.json({ error: console.log(err)})
+           } else {
+             res.status(200).json({
+               msg: `Thanks for updating your profile`
+             });
+           }
         })
-       } else {
-         res.status(200).json({
-           msg: `Thanks for updating your profile`
-         });
-       }
     })
 })
 
-let Storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads/')
-    },
-    filename: function (req, file, cb) {
-      cb(null, `${Date.now()}_${file.originalname}`)
-    },
-    fileFilter: (req, file, cb) => {
-      const ext = path.extname(file.originalname)
-      //console.log(ext)
-      if (!(ext == '.jpg' || ext == '.png' || ext == '.jpeg')) {
-        return cb(new Error('only jpg, png, mp4 is allowed'), false);
-      }
-      cb(null, true)
-    }
-  })
+// let Storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, 'uploads/')
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, `${Date.now()}_${file.originalname}`)
+//     },
+//     fileFilter: (req, file, cb) => {
+//         const ext = path.extname(file.originalname)
+//         if (ext !== '.jpg' && ext !== '.png' && ext !== '.jpeg') {
+//             return cb(res.status(400).end('Only jpg, png, jpeg are allowed'), false)
+//         }
+//         cb(null, true)
+//     }
+// })
 
-const uploads = multer({ storage: Storage })
+// const uploads = multer({ storage: Storage })
 
-const uploaded = uploads.fields([{ name: 'caccertificate', maxCount: 1}, { name: 'idcard', maxCount: 1}, { name: 'bill', maxCount: 1}, { name: 'passport', maxCount: 1}])
+// const uploaded = uploads.fields([{ name: 'caccertificate', maxCount: 1}, { name: 'idcard', maxCount: 1}, { name: 'bill', maxCount: 1}, { name: 'passport', maxCount: 1}])
 
-router.post("/companyUpdate", uploaded, (req, res) => {
+router.post("/companyUpdate", (req, res) => {
     const { user_id, companyname, companyaddress, homeaddress, alternatephone, localgov, State, identity, talk } = req.body
-    const { caccertificate, idcard, passport, bill } = req.files
+    //const { caccertificate, idcard, passport, bill } = req.files
     
-    User.findOneAndUpdate(user_id, { companyname, companyaddress, homeaddress, alternatephone, localgov, State, identity, talk }, function (err, user) {
+    User.findById(user_id, function (err, user) {
         if (err || !user) {
-           return res.status(400).json({
-            msg: "User does not exist"
+            return res.status(400).json({
+                msg: "User does not exist"
+            })
+        }
+    
+        user.updateOne({ companyname, companyaddress, homeaddress, alternatephone, localgov, State, identity, talk }, (err, success) => {
+            if (err) {
+               return res.json({ error: console.log(err)})
+           } else {
+             res.status(200).json({
+               msg: `Thanks for updating your company profile`
+             });
+           }
         })
-       } else {
-         res.status(200).json({
-           msg: `Thanks for updating your profile`
-         });
-       }
     })
 })
 
