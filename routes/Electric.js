b0bd0ba558c8dc540a26b5f05630ce6373f1546f
chika334 <ikenyedaniel@gmail.com>
2020-12-18 08:48:15 +0100

    final

--- routes/Electric.js
+++ routes/Electric.js
@@ -14,7 +14,6 @@ router.get('/verifyNumber', auth, async (req, res) => {
 
 router.post('/verifyNumber', auth, async (req, res, error) => {
 	const { meter, service, select, transactionId } = req.body
-	//console.log(req.body)
 	
 	const user = `${process.env.email_login}:${process.env.password_login}`
 	const base64 = Buffer.from(user).toString('base64');
@@ -30,8 +29,6 @@ router.post('/verifyNumber', auth, async (req, res, error) => {
 		serviceID: service,
 		type: select
 	}
-
-	console.log(body);
 	
 	const userId = await Wallet.findById(req.user.walletId)
 	
@@ -45,9 +42,8 @@ router.post('/verifyNumber', auth, async (req, res, error) => {
 				walletId: userId._id,
 				select: select
 			})
-			
+			verify.save();
 			if(response.data.content.WrongBillersCode == false) {
-				verify.save();
 				res.status(200).json({
 					verify,
 					success: true,
