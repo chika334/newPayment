f89d6df8f488e800b318a136c97d289b08857a7b
chika334 <ikenyedaniel@gmail.com>
2020-12-19 02:17:12 +0100

    all

--- routes/user.js
+++ routes/user.js
@@ -30,14 +30,20 @@ function verifyToken(req, res, next) {
   }
 }
 
-router.get('/me', verifyToken, (req, res, next) => {
+router.get('/me', (req, res, next) => {
   var token = req.headers['x-auth-token'];
   if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
 
   jwt.verify(token, process.env.JWT_SECRET, function(err, decoded) {
     if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
     
-    res.status(200).send(decoded);
+    // res.status(200).send(decoded);
+    User.findById(decoded.id, function (err, user) {
+      if (err) return res.status(500).send("There was a problem finding the user.");
+      if (!user) return res.status(404).send("No user found.");
+      
+      res.status(200).send(user);
+    });
   });
 })
  
