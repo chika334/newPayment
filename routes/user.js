ac515426636bdb736612ad7e3cb4f26c9279d510
chika334 <ikenyedaniel@gmail.com>
2020-12-19 02:42:10 +0100

    all

--- routes/user.js
+++ routes/user.js
@@ -16,8 +16,10 @@ router.get('/auth/me', (req, res) => {
     User.findById(decoded._id, function (err, user) {
       if (err) return res.status(500).send("There was a problem finding the user.");
       if (!user) return res.status(404).send("No user found.");
+
+      const { _id, name, email } = user
       
-      res.status(200).send(user.select('-password -walletId'));
+      res.status(200).send({ user: { _id, name, email } });
     });
   });
 })
