4842e2fbeba9e77d52f39515ab2226d7d8a60020
chika334 <ikenyedaniel@gmail.com>
2020-12-19 02:54:48 +0100

    all

--- routes/user.js
+++ routes/user.js
@@ -81,39 +81,39 @@ router.post('/register', async (req, res) => {
   await user.save();
 
   const token = user.generateAuthToken();
-  res.header('x-auth-token', token).send({ user, token });
+  const { _id, role } = user
+  res.header('x-auth-token', token).send({ user: { _id, name, email, role }, token });
 })
 
 router.post('/updatePassword', async(req, res) => {
-    const { _id, email, old_password, new_password, confirm_new_password } = req.body
-    if ( new_password === "" ) {
-        res.status(400).json({
-            msg: "Input all Fields"
-        })
-    }
-    
-    let user = await User.findOne({ email }, (err, user) => {
-        if (user != null) {
-            var hash = user.password
-            bcrypt.compare(old_password, hash, function(err, res) {
-            if (res) {
-                // Password match
-                if (new_password == confirm_new_password) {
-                    bcrypt.hash(new_password, 10, function(err, hash) {
-                    user.password = hash
-                    user.save()
-                    
-                    })
-                }
-            }          
-            })
-        }
-        res.status(200).json({
-            msg: email + " your password has been changed."
-        })
+  const { _id, email, old_password, new_password, confirm_new_password } = req.body
+  if ( new_password === "" ) {
+    res.status(400).json({
+      msg: "Input all Fields"
     })
-    if (!user) return res.status(400).json({ msg: 'User not found' })
-    
+  }
+  
+  let user = await User.findOne({ email }, (err, user) => {
+    if (user != null) {
+      var hash = user.password
+      bcrypt.compare(old_password, hash, function(err, res) {
+      if (res) {
+        // Password match
+        if (new_password == confirm_new_password) {
+          bcrypt.hash(new_password, 10, function(err, hash) {
+          user.password = hash
+          user.save()
+          
+          })
+        }
+      }          
+      })
+    }
+    res.status(200).json({
+        msg: email + " your password has been changed."
+    })
+  })
+  if (!user) return res.status(400).json({ msg: 'User not found' })
 })
 
 module.exports = router
