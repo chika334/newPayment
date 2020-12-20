4c5718dc7a475ec6acdc1fcab751d01a7bf4e195
chika334 <ikenyedaniel@gmail.com>
2020-12-20 17:18:17 -0600

    all

--- routes/airtime.js
+++ routes/airtime.js
@@ -1,9 +1,9 @@
 const Wallet = require('../model/Wallet')
 const express = require('express')
-const router = express.Router()
+const router = express.Router();
 const auth = require('../middleware/auth')
-const axios = require('axios')
-const Pay = require('../model/PayRequest')
+const axios = require('axios');
+const Pay = require('../model/PayRequest');
 const { v4: uuidv4 } = require('uuid');
 const Transaction = require("../model/Transaction")
 
