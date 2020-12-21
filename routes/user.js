14f82701ca42bc6625247ca7b64a0eff832cd2ee
chika334 <ikenyedaniel@gmail.com>
2020-12-20 19:44:47 -0600

    all

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,25 @@ const router = express.Router()
 const auth = require("../middleware/auth.js")
 const bcrypt = require("bcrypt")
 const Wallet = require('../model/Wallet')
+const jwt = require("jsonwebtoken")
+
+router.get('/auth/me', (req, res) => {
+  var token = req.header('x-auth-token')
+  if (!token) return res.status(401).send({ auth: false, msg: 'No token provided.' });
+
+  jwt.verify(token, process.env.JWT_SECRET, function(err, decoded) {
+    if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+    
+    User.findById(decoded._id, function (err, user) {
+      if (err) return res.status(500).send("There was a problem finding the user.");
+      if (!user) return res.status(404).send("No user found.");
+
+      const { _id, name, email, role } = user
+      
+      res.status(200).send({ user: { _id, name, email, role } });
+    });
+  });
+})
 
 router.get('/getUser', auth, async (req, res) => {
 	const user = await User.findById(req.user._id).select('-password')
@@ -28,7 +47,8 @@ router.post('/login', async (req, res) => {
   if (!validPassword) return res.status(400).json({ msg: 'Invalid Email or Password.' });
 
   const token = user.generateAuthToken();
-  res.send({ user, token })
+  const { _id, name, role } = user
+  res.send({ user: { _id, name, email, role }, token })
 })
 
 // register
@@ -61,39 +81,39 @@ router.post('/register', async (req, res) => {
   await user.save();
 
   const token = user.generateAuthToken();
-  res.header('x-auth-token', token).send({ user, token });
+  const { _id, role } = user
+  res.header('x-auth-token', token).send({ user: { _id, name, email, role }, token });
 })
 
 router.post('/updatePassword', async(req, res) => {
-    const { _id, email, old_password, new_password, confirm_new_password } = req.body
-    if ( new_password === "" ) {
-        res.status(400).json({
-            msg: "Input all Fields"
-        })
-    }
-    
-    let user = await User.findOne({ email }, (err, user) => {
-        if (user != null) {
-            var hash = user.password
-            bcrypt.compare(old_password, hash, function(err, res) {
-            if (res) {
-                // Password match
-                if (new_password == confirm_new_password) {
-                    bcrypt.hash(new_password, 10, function(err, hash) {
-                    user.password = hash
-                    user.save()
-                    
-                    })
-                }
-            }          
-            })
-        }
-        res.status(200).json({
-            msg: email + " your password has been changed."
-        })
+  const { _id, email, old_password, new_password, confirm_new_password } = req.body
+  if ( new_password === "" ) {
+    res.status(400).json({
+      msg: "Input all Fields"
     })
-    if (!user) return res.status(400).json({ msg: 'User not found' })
-    
+  }
+  
+  let user = await User.findOne({ email }, (err, user) => {
+    if (user != null) {
+      var hash = user.password
+      bcrypt.compare(old_password, hash, function(err, res) {
+      if (res) {
+        // Password match
+        if (new_password == confirm_new_password) {
+          bcrypt.hash(new_password, 10, function(err, hash) {
+          user.password = hash
+          user.save()
+          
+          })
+        }
+      }          
+      })
+    }
+    res.status(200).json({
+      msg: email + " your password has been changed."
+    })
+  })
+  if (!user) return res.status(400).json({ msg: 'User not found' })
 })
 
 module.exports = router
