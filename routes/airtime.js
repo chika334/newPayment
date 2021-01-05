aa645dd8e741f6e2464e3bd17a082e7b1abb0e28
chika334 <ikenyedaniel@gmail.com>
2021-01-05 09:58:28 -0600

    done

--- routes/airtime.js
+++ routes/airtime.js
@@ -18,7 +18,7 @@ router.get('/getTransaction', auth, async (req, res) => {
 })
 
 router.post('/creditTransaction', auth, async (req, res) => {
-    const { AmountInt, service, phone, name } = req.body
+    const { amount, service, phone, name } = req.body
     const requestId = uuidv4();
 
     const user = `${process.env.email_login}:${process.env.password_login}`
@@ -40,37 +40,30 @@ router.post('/creditTransaction', auth, async (req, res) => {
     const userId = await Wallet.findById(req.user.walletId)
     console.log(userId)
 
-    /*if(userId.wallet < AmountInt) {
-        res.status(400).json({
-            msg: "Error occured while querying transaction"
+    axios.post(`${process.env.airtime}`, body, config)
+    .then(response => {
+        const transaction = new Transaction({
+            amount: response.data.amount,
+            requestId: response.data.requestId,
+            product_name: response.data.content.transactions.product_name,
+            date: response.data.transaction_date.date,
+            total_amount: response.data.content.transactions.total_amount,
+            transactionId: response.data.content.transactions.transactionId,
+            status: response.data.response_description,
+            walletId: userId._id,
         })
-        return
-    } else {*/
-        axios.post(`${process.env.airtime}`, body, config)
-        .then(response => {
-            const transaction = new Transaction({
-                amount: response.data.amount,
-                requestId: response.data.requestId,
-                product_name: response.data.content.transactions.product_name,
-                date: response.data.transaction_date.date,
-                total_amount: response.data.content.transactions.total_amount,
-                transactionId: response.data.content.transactions.transactionId,
-                status: response.data.response_description,
-                walletId: userId._id,
-            })
 
-            transaction.save();
-            if(response.data.content.transactionId == response.data.content.transactionId) {
-                res.status(200).json({
-                    transaction
-                })
-                return
-            } else {
-                throw err
-            }
-        })
-        .catch(err => console.log(err))
-   // }
+        transaction.save();
+        if(response.data.content.transactionId == response.data.content.transactionId) {
+            res.status(200).json({
+                transaction
+            })
+            return
+        } else {
+            throw err
+        }
+    })
+    .catch(err => console.log(err))
 })
 
 router.post('/Transaction', auth, async (req, res) => {
