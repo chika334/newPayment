8d7912686bdd6a0f97dd35dd599f9b31305ab233
chika334 <ikenyedaniel@gmail.com>
2021-02-22 11:25:38 -0600

    added

--- routes/paystack.js
+++ routes/paystack.js
@@ -3,43 +3,13 @@ const router = express.Router();
 const axios = require('axios');
 const auth = require('../middleware/auth');
 
-router.get(`/paystack`, async (req, res) => {
-	const ref = req.query.reference;
-	let output;
-	await axios
-		.get(`https://api.paystack.co/transaction/verify/${ref}`, {
-			headers: {
-				authorization: 'Bearer sk_test_f85f819b644d84c213774720b1f268abe0e84782',
-				'content-type': 'application/json',
-				'cache-control': 'no-cache'
-			}
-		})
-		.then((success) => {
-			output = success;
-			// console.log(success);
-		})
-		.catch((err) => output = err);
-
-	//now we check for internet connectivity issues
-	// if (!output.response && output.status !== 200) throw new UserInputError('No internet Connection');
-	// //next,we confirm that there was no error in verification.
-	// if (output.response && !output.response.data.status)
-	// 	throw new UserInputError("Error verifying payment , 'unknown Transaction    Reference Id'");
-
-	//  }
-	//we return the output of the transaction
-	res.status(200).send('Payment was successfully verified');
-});
-
-// router.get('/paystack', async (req, res) => {
+// router.get(`/paystack`, async (req, res) => {
 // 	const ref = req.query.reference;
 // 	let output;
 // 	await axios
 // 		.get(`https://api.paystack.co/transaction/verify/${ref}`, {
 // 			headers: {
 // 				authorization: 'Bearer sk_test_f85f819b644d84c213774720b1f268abe0e84782',
-// 				//replace TEST SECRET KEY with your actual test secret
-// 				//key from paystack
 // 				'content-type': 'application/json',
 // 				'cache-control': 'no-cache'
 // 			}
@@ -48,17 +18,47 @@ router.get(`/paystack`, async (req, res) => {
 // 			output = success;
 // 			// console.log(success);
 // 		})
-// 		.catch((error) => {
-// 			output = error;
-// 		});
+// 		.catch((err) => output = err);
+
 // 	//now we check for internet connectivity issues
 // 	// if (!output.response && output.status !== 200) throw new UserInputError('No internet Connection');
 // 	// //next,we confirm that there was no error in verification.
 // 	// if (output.response && !output.response.data.status)
 // 	// 	throw new UserInputError("Error verifying payment , 'unknown Transaction    Reference Id'");
 
+// 	//  }
 // 	//we return the output of the transaction
 // 	res.status(200).send('Payment was successfully verified');
 // });
+
+router.get('/paystack', async (req, res) => {
+	const ref = req.query.reference;
+	let output;
+	await axios
+		.get(`https://api.paystack.co/transaction/verify/${ref}`, {
+			headers: {
+				authorization: 'Bearer sk_test_f85f819b644d84c213774720b1f268abe0e84782',
+				//replace TEST SECRET KEY with your actual test secret
+				//key from paystack
+				'content-type': 'application/json',
+				'cache-control': 'no-cache'
+			}
+		})
+		.then((success) => {
+			output = success;
+			// console.log(success);
+		})
+		.catch((error) => {
+			output = error;
+		});
+	//now we check for internet connectivity issues
+	// if (!output.response && output.status !== 200) throw new UserInputError('No internet Connection');
+	// //next,we confirm that there was no error in verification.
+	// if (output.response && !output.response.data.status)
+	// 	throw new UserInputError("Error verifying payment , 'unknown Transaction    Reference Id'");
+
+	//we return the output of the transaction
+	res.status(200).send('Payment was successfully verified');
+});
 
 module.exports = router;
