f27d2a88d2a7b195415a9f2abdaaf3a42a94142d
chika334 <ikenyedaniel@gmail.com>
2021-03-15 23:23:26 -0500

    changing from paystack to flutterwave

--- routes/paystack.js
+++ routes/paystack.js
@@ -3,33 +3,34 @@ const router = express.Router();
 const axios = require('axios');
 const auth = require('../middleware/auth');
 
-// router.get('/paystack', async (req, res) => {
-// 	const ref = req.query.reference;
-// 	let output;
-// 	await axios
-// 		.get(`https://api.paystack.co/transaction/verify/${ref}`, {
-// 			headers: {
-// 				authorization: 'Bearer sk_test_f85f819b644d84c213774720b1f268abe0e84782',
-// 				//replace TEST SECRET KEY with your actual test secret
-// 				//key from paystack
-// 				'content-type': 'application/json',
-// 				'cache-control': 'no-cache'
-// 			}
-// 		})
-// 		.then((success) => {
-// 			output = success;
-// 			// console.log(success);
-// 		})
-// 		.catch((error) => {
-// 			output = error;
-// 		});
+router.get('/paystack', async (req, res) => {
+	console.log(req);
+	const ref = req.query.reference;
+	let output;
+	await axios
+		.get(`https://api.paystack.co/transaction/verify/${ref}`, {
+			headers: {
+				authorization: 'Bearer sk_test_f85f819b644d84c213774720b1f268abe0e84782',
+				//replace TEST SECRET KEY with your actual test secret
+				//key from paystack
+				'content-type': 'application/json',
+				'cache-control': 'no-cache'
+			}
+		})
+		.then((success) => {
+			output = success;
+			// console.log(success);
+		})
+		.catch((error) => {
+			output = error;
+		});
 
-// 	//we return the output of the transaction
-// 	res.status(200).send('Payment was successfully verified');
-// });
+	//we return the output of the transaction
+	res.status(200).send('Payment was successfully verified');
+});
 
 
-router.post('/flutterwave', async (req, res) => {
+router.get('/flutterwave', async (req, res) => {
 	console.log(req);
 })
 
