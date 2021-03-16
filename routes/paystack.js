7777f13fda5f62fe9cb8a48a88eea4f927e55f2e
chika334 <ikenyedaniel@gmail.com>
2021-03-15 23:11:10 -0500

    changing from paystack to flutterwave

--- routes/paystack.js
+++ routes/paystack.js
@@ -3,13 +3,15 @@ const router = express.Router();
 const axios = require('axios');
 const auth = require('../middleware/auth');
 
-// router.get(`/paystack`, async (req, res) => {
+// router.get('/paystack', async (req, res) => {
 // 	const ref = req.query.reference;
 // 	let output;
 // 	await axios
 // 		.get(`https://api.paystack.co/transaction/verify/${ref}`, {
 // 			headers: {
 // 				authorization: 'Bearer sk_test_f85f819b644d84c213774720b1f268abe0e84782',
+// 				//replace TEST SECRET KEY with your actual test secret
+// 				//key from paystack
 // 				'content-type': 'application/json',
 // 				'cache-control': 'no-cache'
 // 			}
@@ -18,48 +20,17 @@ const auth = require('../middleware/auth');
 // 			output = success;
 // 			// console.log(success);
 // 		})
-// 		.catch((err) => output = err);
+// 		.catch((error) => {
+// 			output = error;
+// 		});
 
-// 	//now we check for internet connectivity issues
-// 	// if (!output.response && output.status !== 200) throw new UserInputError('No internet Connection');
-// 	// //next,we confirm that there was no error in verification.
-// 	// if (output.response && !output.response.data.status)
-// 	// 	throw new UserInputError("Error verifying payment , 'unknown Transaction    Reference Id'");
-
-// 	//  }
 // 	//we return the output of the transaction
 // 	res.status(200).send('Payment was successfully verified');
 // });
 
-router.get('/paystack', async (req, res) => {
-	const ref = req.query.reference;
-	let output;
-	await axios
-		.get(`https://api.paystack.co/transaction/verify/${ref}`, {
-			headers: {
-				authorization: 'Bearer sk_test_f85f819b644d84c213774720b1f268abe0e84782',
-				//replace TEST SECRET KEY with your actual test secret
-				//key from paystack
-				'content-type': 'application/json',
-				'cache-control': 'no-cache'
-			}
-		})
-		.then((success) => {
-			output = success;
-			// console.log(success);
-		})
-		.catch((error) => {
-			output = error;
-		});
 
-	//now we check for internet connectivity issues
-	// if (!output.response && output.status !== 200) res.send('No internet Connection');
-	// //next,we confirm that there was no error in verification.
-	// if (output.response && !output.response.data.status)
-	// 	res.send("Error verifying payment , 'unknown Transaction    Reference Id'");
-
-	//we return the output of the transaction
-	res.status(200).send('Payment was successfully verified');
-});
+router.get('/flutterwave', async (req, res) => {
+	console.log(req.query.reference);
+})
 
 module.exports = router;
